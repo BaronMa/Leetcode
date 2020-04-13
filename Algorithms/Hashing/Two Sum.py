3cde6577b8cadfee4063b260f030325b4f1e986c
BaronMa <41691286+BaronMa@users.noreply.github.com>
2020-04-13 11:52:33 -0400

    Update and rename Algorithms/Two Sum.py to Algorithms/Hashing/Two Sum.py

--- Algorithms/Hashing/Two Sum.py
+++ Algorithms/Hashing/Two Sum.py
@@ -8,5 +8,8 @@ def twoSum(self, nums: List[int], target: int) -> List[int]:
                 dict[value] = index
             else:
                 return [dict[target - value],index]
+# Store all the values as key and their corresponding index as value in a dictionary, then each time, we only need to check if 
+# the difference between target and value is in the dictionary or not, if not, assign the new value and index pair to the 
+# dictionary, if so, return the difference's value and the index.
                 
 # Runtime beats 57.3% of python3 submissions, memory usage beats 66.05% of python3 submissions.
